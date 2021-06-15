385231e191eb5bff012251bc9bb57a7c10c6c03c
mohdmuiz11 <muiz_00@protonmail.com>
2021-06-16 01:08:46 +0800

    Init authentication

--- techplanet/database/migrations/2014_10_12_000000_create_users_table.php
+++ techplanet/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,14 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fname');
-            $table->string('lname');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->text('address');
-            $table->string('phone_no');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
